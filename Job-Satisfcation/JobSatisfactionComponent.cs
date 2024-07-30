c0ae810b6f954b9426573cf1bcc0fdbe539e1064
Luca Noviello <novielloluca12@gmail.com>
2024-07-30 10:06:58 +0200

    Add project files.

---
+++ Job-Satisfcation/JobSatisfactionComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Job_Satisfcation.NoMultipleThoughts;
+using Verse;
+
+namespace Job_Satisfcation
+{
+    internal class JobSatisfactionComponent
+    {
+
+    public class JobSatisfactionGameComponent : GameComponent
+        {
+            public JobSatisfactionGameComponent(Game game)
+            {
+            }
+
+            public override void FinalizeInit()
+            {
+                base.FinalizeInit();
+                AddCompToAllPawns();
+            }
+
+            private void AddCompToAllPawns()
+            {
+                foreach (var pawn in Find.WorldPawns.AllPawnsAlive)
+                {
+                    if (pawn.TryGetComp<CompLastThought>() == null)
+                    {
+                        pawn.AllComps.Add(new CompLastThought());
+                    }
+                }
+
+                Log.Message("JobSatisfaction: Component added to all pawns.");
+            }
+        }
+
+}
+}
---
+++ Job-Satisfcation/NoMultipleThoughts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Job_Satisfcation
+{
+    internal class NoMultipleThoughts
+    {
+
+        public class CompLastThought : ThingComp
+    {
+            public int lastThoughtTick = -1;
+
+            public override void PostExposeData()
+            {
+                base.PostExposeData();
+                Scribe_Values.Look(ref lastThoughtTick, "lastThoughtTick", -1);
+            }
+        }
+
+    public class CompProperties_LastThought : CompProperties
+    {
+        public CompProperties_LastThought()
+        {
+            this.compClass = typeof(CompLastThought);
+        }
+    }
+
+}
+}
